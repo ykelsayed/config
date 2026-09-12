8cf2424fbc71bd1a20d212fc53d169cc9f485def
Yahia <ykelsayed@icloud.com>
2026-09-12 01:20:00 -0600

    Install Hack Nerd Font system-wide

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-{ user, ... }:
+{ user, pkgs, ... }:
 
 {
   # Determinate already manages the Nix daemon, so nix-darwin shouldn't.
@@ -6,7 +6,9 @@
 
   nixpkgs.config.allowUnfree = true;
   nixpkgs.hostPlatform = "aarch64-darwin"; # use x86_64-darwin for Intel CPU
-
+ fonts.packages = [
+  pkgs.nerd-fonts.hack
+];
   system.primaryUser = user;
   users.users.${user} = {
     home = "/Users/${user}";
